818ef2c7564be969f3864caf8fb390cd7d1c7602
Richard <mugrich007@gmail.com>
2025-12-31 20:34:32 +0000

    feat(books): fetch and create practice and/or page when not found

--- client/src/app/(workspaces)/(solo-player)/practice-with-books/practice/page.tsx
+++ client/src/app/(workspaces)/(solo-player)/practice-with-books/practice/page.tsx
@@ -1,4 +1,12 @@
-import { createPracticeTracking, fetchPracticeBookPage, fetchPracticeData, fetchPracticeTracking } from "@/lib/data"
+import { 
+    createPracticeBookPage, 
+    createPracticePlan, 
+    createPracticeTracking, 
+    fetchPracticeBookPage, 
+    fetchPracticePlan, 
+    fetchPracticeTracking 
+} from "@/lib/data"
+
 import PracticeClient from "./PracticeClient"
 // import { updatePracticeTracking } from "@/lib/actions"
 
@@ -8,15 +16,22 @@ const page = async ({
     searchParams: Promise<{bookID: string, page?: number, score?: number}>
 }) => {
     const { bookID, page: optionalPageNumber, score } = await searchParams
-    const { pageContent, pageNumber } = (await fetchPracticeBookPage(bookID, optionalPageNumber)) ?? {}
-    if (!pageContent) {
-        let practice = await fetchPracticeData(bookID)
-        if (!practice) {
-            // create practice plan
-            practice = await createPracticePlan(bookID)
-        }
-        return <div>Error finding practice page</div>
+    let [practicePlan, practicePage] = await Promise.all([fetchPracticePlan(bookID), fetchPracticeBookPage(bookID, optionalPageNumber)])
+    // const { pageContent, pageNumber } = (await fetchPracticeBookPage(bookID, optionalPageNumber)) ?? {}
+    if (!practicePage) {
+        console.warn("Couldn't find the practice page")
+        if (!practicePlan) {
+            console.warn("Couldn't find the practice plan either...Attempting to create one") //! So make sure you don't duplicate it in case of errors
+            practicePlan = await createPracticePlan(bookID)
+            console.log("Practice plan: ", practicePlan)
+        } 
+        if (!practicePlan) return <div>Error creating practice page</div>
+        // Creating the page when we have the practice plan
+        console.warn('Creating practice page....')
+        practicePage = await createPracticeBookPage(bookID, optionalPageNumber) //! Make sure you don't do this at the end of the book
+        if (!practicePage) return <div>Error finding practice page</div>
     }
+    console.log(practicePlan, practicePage)
     // await updatePracticeTracking(score)
 
   return (
@@ -29,7 +44,7 @@ const page = async ({
   
         {/* Your Content Container */}
         <div className="absolute inset-0 w-full h-screen flex justify-center px-10"  id="book">
-            <PracticeClient bookID={bookID} page={pageContent} pageNumber={Number(optionalPageNumber ?? pageNumber)} score={Number(score)}/>
+            <PracticeClient bookID={bookID} page={practicePage.pageContent} pageNumber={Number(optionalPageNumber ?? practicePage.pageNumber)} score={Number(score)}/>
         </div>
     </div>
   )
