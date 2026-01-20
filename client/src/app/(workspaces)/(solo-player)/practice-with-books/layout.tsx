21725e50eb06465c9cc0d199f188d2b25ee3d069
Richard <mugrich007@gmail.com>
2026-01-20 04:54:24 +0000

    feat(books): style the practice content box to fill with on small screens

--- client/src/app/(workspaces)/(solo-player)/practice-with-books/layout.tsx
+++ client/src/app/(workspaces)/(solo-player)/practice-with-books/layout.tsx
@@ -4,49 +4,18 @@ import { Separator } from "@radix-ui/react-separator"
 import { ReactNode } from "react"
 
 const layout = ({ children }: { children: ReactNode}) => {
-    const breadcrumbs: {name: string, slug: string}[] = []
 
   return (
     <>
-      {/* <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 fixed z-3">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator
               orientation="vertical"
               className="mr-2 data-[orientation=vertical]:h-4"
             />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="/topics">
-                    Books
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                {
-                    breadcrumbs.map((bc, i) => (
-                        <BreadcrumbItem key={bc.slug}>
-                            <BreadcrumbSeparator className="hidden md:block" />
-                            { (i === breadcrumbs.length - 1) ? (
-                                <BreadcrumbPage>{bc.name}</BreadcrumbPage>
-                                ) : (
-                                    <BreadcrumbLink
-                                        href={`
-                                            /topics/${breadcrumbs
-                                                .slice(0, i+1)
-                                                .map(bc => bc.slug)
-                                                .join('/')}
-                                        `}
-                                    >{bc.name}</BreadcrumbLink>
-                                )
-
-                            }
-                        </BreadcrumbItem>
-                    ))
-                }
-              </BreadcrumbList>
-            </Breadcrumb>
           </div>
-        </header> */}
+        </header>
         <div className="flex flex-1 flex-col gap-4">
           {/* <div className="grid auto-rows-min gap-4 md:grid-cols-3">
             <div className="bg-muted/50 aspect-video rounded-xl" />
--- client/src/app/(workspaces)/(solo-player)/practice-with-books/practice/PracticeClient.tsx
+++ client/src/app/(workspaces)/(solo-player)/practice-with-books/practice/PracticeClient.tsx
@@ -11,49 +11,29 @@ import { useEffect, useState } from "react"
 import { ArrowLeft, ArrowRight, RefreshCcw } from "lucide-react"
 
 const PracticeClient = (
-    { page, bookId, pageNumber, score: initialScore, topicId, wordsPerPage }: 
-    { page: PracticeBookPage, bookId: string, pageNumber: number, score: number, topicId?: string, wordsPerPage?: number}
+    { page, bookId, pageNumber, score: initialScore, topicId, wordsPerPage }:
+        { page: PracticeBookPage, bookId: string, pageNumber: number, score: number, topicId?: string, wordsPerPage?: number }
 ) => {
 
     const { allPs, currentStreak, score, currentPidx, solvedWords, handleWordChoice } = usePractice(initialScore, page)
     const [currentPage, setCurrentPage] = useState(pageNumber)
 
-  return (
-    <div className="w-full">
-        <div className="flex justify-end h-[5%] items-center w-full">
-            <span className="flex gap-2 relative" id="streak">
-                { currentStreak >= 3 ? "💎" : 
-                    currentStreak === 2 ? "🔥" :
-                    currentStreak ? "⭐" : "💣"
+    return (
+        <div className="w-full">
+            <div className="flex justify-end h-[5%] items-center w-full">
+                <span className="flex gap-2 relative" id="streak">
+                    {currentStreak >= 3 ? "💎" :
+                        currentStreak === 2 ? "🔥" :
+                            currentStreak ? "⭐" : "💣"
 
-                }
-            </span>
-            <span className="ml-10">{score}🪙</span>
-        </div>
-        <div className="p-3 flex h-[95%] w-full gap-5">
-            <Card className="w-100 h-fit bg-card/50 backdrop-blur-md">
-                <CardHeader>
-                    <CardTitle>Key words:</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <div className="flex flex-wrap gap-2 mt-2">
-                        {page.options.map((w, i) => (
-                            <Badge 
-                                key={`${w}-${i}`} 
-                                onClick={() => handleWordChoice(w)} 
-                                variant="secondary" 
-                                className="cursor-pointer"
-                            >
-                                {w}
-                            </Badge>
-                        ))}
-                    </div>
-                </CardContent>
-            </Card>
-            
-            <Card className="w-200 bg-card/50 backdrop-blur-2xl pl-10">
-                <ScrollArea className="h-full overflow-hidden">
-                    <CardContent className="text-xl font-serif leading-8 text">
+                    }
+                </span>
+                <span className="ml-10">{score}🪙</span>
+            </div>
+            <div className="p-3 flex h-[95%] w-full gap-5 justify-center flex-wrap">
+                <Card className="w-[95dvw] lg:w-[800px] lg:shrink-0 h-full bg-card/50 backdrop-blur-2xl pl-10 ">
+                    <ScrollArea className="flex-1 overflow-hidden">
+                        <CardContent className="text-xl font-serif leading-8 text">
                             {allPs.map((pg, idx) => {
                                 let displayedText = pg
                                 if (idx === currentPidx) {
@@ -64,28 +44,48 @@ const PracticeClient = (
                                     // Leave future ps untouched. To focus on the current
                                     // displayedText
                                 }
-                                
+
                                 return <p key={idx} className={
-                                        displayedText[0] === displayedText[0]?.toUpperCase() ? 'mt-5' : 'mt-.5'
-                                    }>{displayedText}</p>
+                                    displayedText[0] === displayedText[0]?.toUpperCase() ? 'mt-5' : 'mt-.5'
+                                }>{displayedText}</p>
                             })}
+                        </CardContent>
+                        <ScrollBar orientation='vertical' />
+                    </ScrollArea>
+                    <CardFooter className="flex justify-center">
+                        <div className="flex gap-5 items-center">
+                            <Link className="bg-secondary p-1 pl-3 rounded-l-md" href={`practice?bookId=${bookId}&page=${currentPage}&score=${score}`}><RefreshCcw /></Link>
+                            <Link className="bg-secondary p-1 pl-3 rounded-l-md" href={`practice?bookId=${bookId}&page=${currentPage - 1}&score=${score}`}><ArrowLeft /></Link>
+                            <input onChange={e => setCurrentPage(Number(e.target.value))} value={currentPage} className="w-10" />
+                            {currentPidx >= allPs.length &&
+                                <Link className="bg-secondary p-1 pr-3 rounded-r-md" href={`practice?bookId=${bookId}&page=${currentPage + 1}&score=${score}&topicId=${topicId}&wordsPerPage=${wordsPerPage || 2}`}><ArrowRight /></Link>
+                            }
+                        </div>
+                    </CardFooter>
+                </Card>
+
+                <Card className="w-100 h-fit bg-card/50 backdrop-blur-md">
+                    <CardHeader>
+                        <CardTitle>Key words:</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="flex flex-wrap gap-2 mt-2">
+                            {page.options.map((w, i) => (
+                                <Badge
+                                    key={`${w}-${i}`}
+                                    onClick={() => handleWordChoice(w)}
+                                    variant="secondary"
+                                    className="cursor-pointer"
+                                >
+                                    {w}
+                                </Badge>
+                            ))}
+                        </div>
                     </CardContent>
-                    <ScrollBar orientation='vertical' />
-                </ScrollArea>
-                <CardFooter className="flex justify-center">
-                    <div className="flex gap-5 items-center">
-                        <Link className="bg-secondary p-1 pl-3 rounded-l-md" href={`practice?bookId=${bookId}&page=${currentPage}&score=${score}`}><RefreshCcw /></Link>
-                        <Link className="bg-secondary p-1 pl-3 rounded-l-md" href={`practice?bookId=${bookId}&page=${currentPage - 1}&score=${score}`}><ArrowLeft /></Link>
-                        <input onChange={e => setCurrentPage(Number(e.target.value))} value={currentPage} className="w-10"/>
-                        { currentPidx >= allPs.length &&
-                            <Link className="bg-secondary p-1 pr-3 rounded-r-md" href={`practice?bookId=${bookId}&page=${currentPage + 1}&score=${score}&topicId=${topicId}&wordsPerPage=${wordsPerPage || 2}`}><ArrowRight /></Link>
-                        }
-                    </div>
-                </CardFooter>
-            </Card>
+                </Card>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default PracticeClient
